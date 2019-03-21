7a99d13c2f9145284d60d7c5de444f201c1dcbf1
seyfiaslann <seyfi.aslan@brandbastion.com>
2019-03-21 11:03:22 +0000

    Initial commit

---
+++ src/hello.js
@@ -0,0 +1,11 @@
+exports.handler = function (event, context, callback) {
+    const response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header": "My Header Value"
+        },
+        body: JSON.stringify({"message": "Hello World!"})
+    };
+
+    callback(null, response);
+};
