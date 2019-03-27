785615624b4932884b528b745479c61bc07abd4a
seyfiaslann <seyfi.aslan@brandbastion.com>
2019-03-27 14:50:24 +0300

    Added env variable to header for test

--- src/hello.js
+++ src/hello.js
@@ -2,7 +2,7 @@ exports.handler = function (event, context, callback) {
     const response = {
         statusCode: 200,
         headers: {
-            "x-custom-header": "My Header Value"
+            "x-custom-header": "My Header Value - " + process.env.secret_variable
         },
         body: JSON.stringify({"message": "Hello World!?"})
     };
